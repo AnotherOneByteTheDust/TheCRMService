b4cb92abfe4298fe4f9477c82b4aa252392edbb5
Kevin Eliezer <kevineli.gp@gmail.com>
2021-08-25 22:21:32 +0100

    feat application logger

---
+++ src/common/logger/log4js/index.ts
@@ -0,0 +1,6 @@
+import { logger } from './config'
+
+type LogTypes = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal' | 'mark'
+
+export const appLogger = (logType: LogTypes, message: string) => logger[logType](`[application] - ${message}`)
+export const serverLogger = (logType: LogTypes, message: string) => logger[logType](`[server] - ${message}`)
