044484a0dfc38abe87faf24d65d3f06f1052ccce
Kevin Eliezer <kevineli.gp@gmail.com>
2021-09-05 19:55:17 +0100

    feat getcustomer and deletecustomer queries

--- src/infrastructure/orm/mongoose/requests/customer.mongodb.requests.ts
+++ src/infrastructure/orm/mongoose/requests/customer.mongodb.requests.ts
@@ -1,7 +1,20 @@
-import { NewCustomerDatabaseDto } from '@infrastructure/dtos/customer.dtos'
+import { CustomerDto, NewCustomerDatabaseDto } from '@infrastructure/dtos/customer.dtos'
 import { Customer } from '../models'
 
 export const create = async (newCustomerData: NewCustomerDatabaseDto): Promise<void> => {
   // Not saving data into database!
   await (new Customer(newCustomerData)).save()
+}
+
+export const getCustomerById = async (id: string): Promise<CustomerDto | null> => {
+  return Customer.findById(id).select('-_id ').lean<CustomerDto>()
+}
+
+export const getCustomers = async (): Promise<CustomerDto[]> => {
+  return Customer.find({}).lean<CustomerDto[]>()
+}
+
+export const deleteCustomer = async (customerId: string): Promise<void> => {
+  const conditions = { _id: customerId }
+  await Customer.deleteOne(conditions)
 }
